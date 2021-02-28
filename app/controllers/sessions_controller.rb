ac5780d05a8633bd581d179d70807418ab12bf91
Sinrock <mruhaak@gmail.com>
2021-02-28 00:58:14 -0700

    worked on controllers and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,17 @@ class SessionsController < ApplicationController
     end
 
     def create
+        u = User.find_by_email(params[:email])
+        if u && u.authenticate(params[:password])
+            session[:user_id] = u.id
+            redirect_to user_path(u)
+        else
+            flash[:message] = "Incorrect email or password.  Please try again."
+        end
     end
 
     def destroy
+        session.delete(:user_id)
+        redirect_to '/login'
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :password)
+      params.require(:user).permit(:user_name, :password)
     end
 
 end
