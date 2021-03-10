5726d1a652d5feeac12ffe6b9b3f89124348b452
Sinrock <mruhaak@gmail.com>
2021-03-09 22:25:54 -0700

    fixing authentication in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,37 @@ class SessionsController < ApplicationController
     end
 
     def create
-        u = User.find_by_email(params[:email])
-        if u && u.authenticate(params[:password])
-            session[:user_id] = u.id
-            redirect_to user_path(u)
+        @user = User.find_by(email: params[:user][:email])
+        if @user.try(:authenticate, params[:user][:password])
+        #if @user && @user.authenticate(params[:user][password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
-            flash[:message] = "Incorrect email or password.  Please try again."
+            flash[:error] = "Invalid information entered, please try again."
+            redirect_to login_path
         end
     end
+
+    #if @user.save
+     #   session[:user.id] = @user.id
+     #   redirect_to user_path(@user)
 
     def destroy
         session.delete(:user_id)
         redirect_to '/'
     end
 end
+
+#
+
+
+#def create
+#    u = User.find_by_email(params[:email])
+#    if u && u.authenticate(params[:password])
+#        session[:user_id] = u.id
+#        redirect_to user_path(u)
+#    else
+#        flash[:message] = "Incorrect email or password.  Please try again."
+#        redirect_to '/login'
+#    end
+#end
