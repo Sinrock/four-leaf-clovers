93dff9e4cafe162061a7747c9bcbed13945bceec
Sinrock <mruhaak@gmail.com>
2021-04-05 23:25:20 -0600

    added nav bar for user paths and cleaned up welcome and show pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
-
     protect_from_forgery with: :exception
-
+    
     include ApplicationHelper
 
+    helper_method :current_user, :logged_in? #now I can use these helper methods in the views as well, hot dog!
+
+    private
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
-    def logged_in
+    def logged_in?
         !!session[:user_id]
     end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_logged_in
 
     def index
     end
