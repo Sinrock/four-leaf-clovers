4bbf248bb23530d8f1b5a7f5c106e1c99b42cc9e
Sinrock <mruhaak@gmail.com>
2021-08-29 15:33:33 -0600

    setup config testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,7 +35,7 @@ class SessionsController < ApplicationController
     #    flash[:error] = "Credentials Error"
     #    redirect_to login_path
     #   end
-    #end
+    #end (changes added here to verify setup)
 
     def create
         @user = User.find_by_email(params[:email])
